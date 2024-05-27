d7992143937a96f00c8f69cd9edccdc5fe00e091
Shivika <vg207765@gmail.com>
2024-05-27 11:32:55 +0530

    contctUpdate

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -55,6 +55,25 @@ function Contact() {
       />
       {/* contact part */}
       <div className='min-h-screen w-full lg:flex gap-5 p-8 text-black bg-zinc-300'>
+  <div className='w-[50%]'>
+    <h2 className='font-semibold text-2xl m-10'>Contact Us</h2>
+    <h1 className='font-bold text-4xl lg:text-9xl'>Let's work together</h1>
+  </div>
+  <div className='w-[50%] p-8'>
+    <form ref={form} onSubmit={sendEmail} className='flex flex-col items-start gap-3 justify-start'>
+      <label className='font-semibold'>First name</label>
+      <input type='text' name='from_name' required className='bg-transparent block outline-none border-b-2 border-gray-600' />
+      <label className='font-semibold'>Last name</label>
+      <input type='text' name='last_name' required className='bg-transparent block outline-none border-b-2 border-gray-600' />
+      <label className='font-semibold'>Email</label>
+      <input type='email' name='from_email' required className='bg-transparent block outline-none border-b-2 border-gray-600' />
+      <label className='font-semibold'>Message</label>
+      <textarea name='message' required className='bg-transparent block outline-none border-2 h-28 border-gray-600'></textarea>
+      <button type='submit' className='bg-blue-600 px-6 py-2 rounded-md m-6 text-white font-extrabold'>Submit</button>
+    </form>
+  </div>
+</div>
+<div className='min-h-screen w-full lg:flex gap-5 p-8 text-black bg-zinc-300'>
         <div className='w-[50%]'>
           <h2 className='font-semibold text-2xl m-10 '>Contact Us</h2>
           <h1 className='font-bold text-4xl lg:text-9xl'>Let's work together</h1>
